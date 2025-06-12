0932a3ece66dcfc72af46a3d8a97c9829bc10bff
Pasindu Isuranga <nanayakkarap93@gmail.com>
2025-06-12 23:11:13 +0530

    type all code

--- logPro.php
+++ logPro.php
@@ -20,6 +20,25 @@
             echo'<h2>Database connection failed</h2>'.mysqli_error(!$conn);
             echo'<a href="index.html"><ion-icon name="arrow-back"></ion-icon></a>';
         }
+        else{
+            $queSel="SELECT RealName FROM users WHERE UserName='$un' AND Password='$pw'";
+            $res=mysqli_query($conn,$queSel);
+            if(mysqli_num_rows($res)>0){
+                $data=mysqli_query($conn,$queSel);
+                $value=(mysqli_fetch_assoc($data));
+				$_SESSION['RN']=$value["RealName"];
+				$rn=$value['RealName'];
+											
+				$queLogUp="INSERT INTO logsch(RealName,Login)VALUES('$rn',(now()))";
+				if(mysqli_query($conn,$queLogUp)){
+				header("Location:Homepage.php");
+                }
+            }
+            else{
+                echo'<h2>Invalid Username or Password</h2>';
+                echo'<a href="index.html"><ion-icon name="arrow-back"></ion-icon></a>';
+            }
+        }
     ?>
 </body>
 </html>
