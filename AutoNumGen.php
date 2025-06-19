c04b45f759092266dde0b08b0d0294052214032b
Pasindu Isuranga <nanayakkarap93@gmail.com>
2025-06-18 20:16:47 -0700

    time zone problem

---
+++ AutoNumGen.php
@@ -0,0 +1,45 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php
+	$conn=mysqli_connect('localhost','root','','bizhub');
+	if(!$conn){
+				echo'<h2>DATABASE ERROR!</h2>Database is not connected!'.mysqli_error($conn);
+				echo'<a href="index.html"><img src="Image/back.phn" width="35" height="35"></a>';
+				}
+				else{
+						$queCh="SELECT MAX(CSerial)FROM cusreg";
+						if(mysqli_query($conn,$queCh)){
+														$res=mysqli_query($conn,$queCh);
+
+														if(mysqli_num_rows($res)>0){
+															$data=mysqli_fetch_array($res);
+															$id=$data['MAX(CSerial)'];
+															$id+=1;
+														}	
+														else{
+															$id=1;
+														}	
+														if($id<'10'){$a='00000';}
+														else if($id<'100'){$a='00000';}
+														else if($id<'1000'){$a='0000';}
+														else if($id<'10000'){$a='000';}
+														else if($id<'100000'){$a='00';}
+														else if($id<'1000000'){$a='';}
+
+														$_SESSION['cid']='BIZ-'.$a.$id;
+														#echo $_SESSION['cid']; #check karanna 
+														header("Location: cusReg.php");
+														}
+
+					}
+?>
+<body>
+</body>
+</html>
--- homepage.php
+++ homepage.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+	session_start();
+	if(isset($_SESSION['RN'])){
+?>
+
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="utf-8">
+<title>BizHubt</title>
+
+<link rel="stylesheet" type="text/css" media="all" href="CSS/mystyle.css">
+
 </head>
+
 <body>
-    
+	<?php
+    	echo'<div class="wrapper">
+			<div class="box header"><img src="Image/head.jpg" width="100%"></div>
+			<div class="box msg"><p align="right" style="fontsize:14px">Welcome to BizHub System,'.$_SESSION['RN'].' (Logout <a href="Login.html"><img src="Image/switch.jpg" width="20" height="20"></a>)</p></div>
+			<div class="box sidebar"><p align="left" style="background:#000033;color:#ffffff;padding:4px">REGISTRATION</p>
+				<p align="right"><a href="AutoNumGen.php" style="width:75%">CUSTOMER</a></p>
+				<p align="right"><a href="bbb.php" style="width:75%">SERVICE</a></p>
+				<p align="left" style="background:#000033;color:#ffffff;p/adding:4px">SEARCH</p>
+				<p align="right"><a href="SearchCus.php" style="width:75%">CUSTOMER</a></p>
+				</div>
+			<div class="box content"><img src="Image/home.jpg" width="100%"></div>
+			<div class="box footer"><p align="center" style="font-size:9px">All Rights Reserved by HND COM SE 037</p></div> 
+				</div>';
+			}
+	?>
 </body>
 </html>
